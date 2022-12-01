5ebcf885f95b797e3445cfd9e42168d52ff6eb05
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-01 23:51:29 +0530

    'uc5 commit'

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,28 +1,60 @@
 public class EmployeeWage {
-    int Wage_per_Hour=20;
-    int Full_Day_Hour=8;
-    int Part_Time=4;
-    public void employee_wage_calculation(){
-        double num=Math.random();
-        int randomCheck=((int) (num *10))%3;
-        System.out.println("Random check value:"+randomCheck);
-        if(randomCheck==2)
-        {
-            System.out.println("employee is present");
-            int employee_Daily_wage=Wage_per_Hour*Full_Day_Hour;
 
-            System.out.println("employee wage is:" +employee_Daily_wage);
-        }else if(randomCheck==1)
-        {
-            System.out.println("employee is present halfday");
-            int employee_Daily_wage=Wage_per_Hour*Part_Time;
-            System.out.println("employee wage is:" +employee_Daily_wage);
-        }else {
-            System.out.println("employee is absent");
+    int Full_Time = 8;
+    int Part_Time = 4;
+    int employee_wage_per_month;
+    int a_count = 0;//absent days count
+    int ft_count = 0;//fulltime days count
+    int pt_count = 0;//part time days count
+     int Wage_per_Hour = 20;
+     int Working_Days_per_month = 20;
+     int randomCheck;
+
+    public EmployeeWage() {
+    }
+
+    public void calculation_of_wage()
+    {
+
+        for (int i = 0; i < 30; i++) {
+            double  randomCheck = Math.floor(Math.random() * 10) % 3;
+            System.out.print("Random check value:" + randomCheck);
+            switch ((int) randomCheck) {
+                case 0:
+                    System.out.print("A-");
+                    a_count++;
+                    break;
+                case 1:
+                    System.out.print("FT-");
+                    ft_count++;
+                    break;
+                case 2:
+                    System.out.print("PT-");
+                    pt_count++;
+                    break;
+                default:
+                    System.out.println("not  usable");
+            }
+            if (((ft_count + pt_count) >= Working_Days_per_month) || ((pt_count * 4 + ft_count * 8) >= 100)) {
+                break;
+            }
+            System.out.println();
         }
     }
-    public  static void main(String args[]){
-        EmployeeWage obj1=new EmployeeWage();
-        obj1.employee_wage_calculation();
+
+    public void DisplayResult() {
+        employee_wage_per_month = (pt_count * Part_Time * Wage_per_Hour) + (ft_count * Full_Time * Wage_per_Hour);
+        System.out.println("total absent days =" + a_count);
+        System.out.println("total full time days =" + ft_count);
+        System.out.println("total part time days =" + pt_count);
+        System.out.println("total wage per month =" + employee_wage_per_month);
+        //* Working_Days_per_month;
+    }
+
+    public static void main(String[] args)
+    {
+        EmployeeWage ob1=new EmployeeWage();
+        ob1.calculation_of_wage();
+        ob1.DisplayResult();
     }
 }
