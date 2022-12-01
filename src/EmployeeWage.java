ac7f3e8a783c8a3eb3fd367c6f10651f4e22fabc
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-01 23:39:22 +0530

    'uc3 commit'

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,15 +1,23 @@
 public class EmployeeWage {
     int Wage_per_Hour=20;
     int Full_Day_Hour=8;
+    int Part_Time=4;
     public void employee_wage_calculation(){
         double num=Math.random();
-        int randomCheck=((int) (num *10))%2;
+        int randomCheck=((int) (num *10))%3;
         System.out.println("Random check value:"+randomCheck);
-        if(randomCheck==1) {
+        if(randomCheck==2)
+        {
             System.out.println("employee is present");
-            int employee_wage=Wage_per_Hour*Full_Day_Hour;
-            System.out.println("employee wage is:" +employee_wage);
-        }else if(randomCheck==0) {
+            int employee_Daily_wage=Wage_per_Hour*Full_Day_Hour;
+
+            System.out.println("employee wage is:" +employee_Daily_wage);
+        }else if(randomCheck==1)
+        {
+            System.out.println("employee is present halfday");
+            int employee_Daily_wage=Wage_per_Hour*Part_Time;
+            System.out.println("employee wage is:" +employee_Daily_wage);
+        }else {
             System.out.println("employee is absent");
         }
     }
